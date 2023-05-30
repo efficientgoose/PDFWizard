a809807a986fce6d19413e158f23f043817e5165
Ajinkya Kale <127986789+ajinkode@users.noreply.github.com>
2023-05-30 12:01:20 +0530

    Improved the UI

--- app.py
+++ app.py
@@ -14,11 +14,49 @@ import os
 
 # Coding the sidebar
 
-os.environ['OPENAI_API_KEY']='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'
+
+# st.image('./chat.jpg', width=200, caption="Chat with your PDF")
+
+# # Add custom CSS to center the image
+# st.markdown(
+#     """
+#     <style>
+#     .center {
+#         display: flex;
+#         justify-content: center;
+#         align-items: center;
+#         height: 100vh;
+#     }
+#     </style>
+#     """,
+#     unsafe_allow_html=True
+# )
+
+# # Display the image in the center
+# st.markdown(
+#     """
+#     <div class="center">
+#         <img src="./chat.jpg" alt="Image" width=200>
+#     </div>
+#     """,
+#     unsafe_allow_html=True
+# )
+
+# Center the image
+container = st.container()
+col_centered = container.columns(3)
+col_centered[1].image('./chat.jpg', width=200)
+
 
 
 with st.sidebar:
     st.title("PDF Wizard 💬🤖")
+    
+    
+    
+    
+    pdf = st.file_uploader("Upload your PDF", type='pdf')
+    
     st.markdown('''
         ## About
         Experience the ultimate PDF companion with our user-friendly LLM-powered chatbot! Effortlessly upload your PDFs and ask any questions related to the PDF.
@@ -27,62 +65,100 @@ with st.sidebar:
     st.write("Made with ❤️ by [Ajinkya Kale](https://www.linkedin.com/in/ajinkode/)")
     
     
-
+    
+    # OPENAI_API_KEY = 'abc'
+# Hide 'Made with Streamlit'
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+    
+    
+    
 def main():
     st.header("Chat with PDF 💬")
     # load_dotenv()
     
     # upload a pdf file
-    pdf = st.file_uploader("Upload your PDF", type='pdf')
+    st.markdown("Get your OpenAI API Key [here](https://platform.openai.com/account/api-keys) ")
+    tempkey = st.text_input(":orange[Please enter your OpenAI API Key]", type='password', placeholder='sk-xxxx')
     
-    if pdf is not None:
-        pdf_reader = PdfReader(pdf)
     
-        text = ""
-        for page in pdf_reader.pages:
-            text+=page.extract_text()
-            
-        # st.write(text)
-        text_splitter = RecursiveCharacterTextSplitter(
-            chunk_size = 1000,
-            chunk_overlap=200,
-            length_function=len
-        )
+    if tempkey is not None:
         
-        chunks = text_splitter.split_text(text=text)
+        apikey = tempkey
+    
+        os.environ['OPENAI_API_KEY']=apikey
         
-        # Create Embeddings
-        store_name = pdf.name[:-4]
+        if apikey:
+            st.success("Successfully uploaded the key! Now please go ahead and upload your PDF.", icon="✅")
+    
+    
+        if pdf is not None:
+            pdf_reader = PdfReader(pdf)
         
-        if os.path.exists(f"{store_name}.pkl"):
-            with open(f"{store_name}.pkl", "rb") as f:
-                VectorStore = pickle.load(f)
-                # st.write("Embeddings loaded from the Disk! Money saved!")
+            text = ""
+            for page in pdf_reader.pages:
+                text+=page.extract_text()
                 
-        else:
-            embeddings = OpenAIEmbeddings()
-            VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
-            with open(f"{store_name}.pkl", "wb") as f:
-                pickle.dump(VectorStore, f)
+            # st.write(text)
+            text_splitter = RecursiveCharacterTextSplitter(
+                chunk_size = 1000,
+                chunk_overlap=200,
+                length_function=len
+            )
+            
+            chunks = text_splitter.split_text(text=text)
+            
+            # st.write(chunks)
+            
+            # Create Embeddings
+            store_name = pdf.name[:-4]
+            
+            if os.path.exists(f"{store_name}.pkl"):
+                with open(f"{store_name}.pkl", "rb") as f:
+                    VectorStore = pickle.load(f)
+                    # st.write("Embeddings loaded from the Disk! Money saved!")
+                    
+            else:
+                embeddings = OpenAIEmbeddings()
+                VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
+                with open(f"{store_name}.pkl", "wb") as f:
+                    pickle.dump(VectorStore, f)
+                    
+                    
+            # Accept user questions/query
+            query = st.text_input("Ask questions about your PDF file: ")
+            # st.write(query)
+            
+            if query:
+                docs = VectorStore.similarity_search(query=query, k=3)
+                
+                # st.write(docs)
+                
+                llm = OpenAI(temperature=0, model_name='gpt-3.5-turbo')
+                chain = load_qa_chain(llm=llm, chain_type="stuff")
                 
                 
-        # Accept user questions/query
-        query = st.text_input("Ask questions about your PDF file: ")
-        # st.write(query)
-        
-        if query:
-            docs = VectorStore.similarity_search(query=query, k=3)
-            
-            llm = OpenAI(temperature=0)
-            chain = load_qa_chain(llm=llm, chain_type="stuff")
-            
-            with get_openai_callback() as cb:
+                
+                if st.button('Tap to see tokens consumed'):
+                    with get_openai_callback() as cb:
+                        response = chain.run(input_documents=docs, question=query)
+                        st.write("Total tokens and total cost associated: ")
+                        st.write(cb)
+                        
+                        # response = chain.run(input_documents=docs, question=query)
+                    st.subheader("Result: ")
+                    st.info(response, icon='ℹ️')
+                        
+                
                 response = chain.run(input_documents=docs, question=query)
-                # st.write("Total tokens and total cost associated: ")
-                st.write(cb)
-            
-            st.subheader("Result: ")
-            st.write(response)
+                st.subheader("Result: ")
+                st.info(response, icon='ℹ️')
             
                 
                 
